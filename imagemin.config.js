aae11ef3dc8cddb28224907e5f2504bc27e20846
Mia Magalhães <hmilena@MBP-de-Mia.home>
2021-04-28 19:34:55 +0100

    no source maps on build, fix png result quality

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "gifsicle": { "optimizationLevel": 6, "interlaced": false, "colors": 10 },
     "mozjpeg": { "progressive": true, "quality": 50 },
-    "pngquant": { "quality": [0.50, 0.50] },
+    "pngquant": { quality: [0.6, 0.8] },
     "svgo": {
       "plugins": [
         { "removeViewBox": false },
